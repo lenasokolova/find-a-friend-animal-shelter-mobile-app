68c6d0bf3bd38b56e7f3c4a931a1d7903e7b9432
Lena Sokolova <sokolhelena94@gmail.com>
2023-01-11 10:45:24 +0100

    style: add styles to the Sections component

--- components/Sections.js
+++ components/Sections.js
@@ -1,20 +1,30 @@
 import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
 import SectionCard from './SectionCard';
+import { AntDesign } from '@expo/vector-icons';
 
 const Sections = () => {
     return (
-        <ScrollView
-            showsHorizontalScrollIndicator={false}
-            contentContainerStyle={{
-                paddingHorizontal: 15,
-                paddingTop: 10,
-            }}
-        >
-            <SectionCard />
-            <SectionCard />
+        <View classname="">
+            <View className="mt-4 ">
+                <Text className="font-bold text-lg">Shelters near you</Text>
+            </View>
+            <Text className="text-xs text-gray-500 py-1">Here you can find animal shelters in your area</Text>
+            <ScrollView
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{
+                    paddingTop: 10,
+                }}
+                classname="flex-1"
+            >
+                <SectionCard title="Wiskers Heaven Shelter" />
+                <SectionCard title="Find-a-Fren Shelter" />
+                <SectionCard title="Paws'n'Claws Shelter" />
+                <SectionCard title="Wiskers Heaven Shelter" />
 
-        </ScrollView>
+            </ScrollView>
+        </View>
     )
 }
 
