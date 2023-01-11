8934989a50b819ecca9612a2f24d7251c91c30a6
Lena Sokolova <sokolhelena94@gmail.com>
2023-01-11 14:13:25 +0100

    style: change bgs + add icons, general styles

--- components/SectionCard.js
+++ components/SectionCard.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 
 import { AntDesign } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 const SectionCard = ({ title, imgUrl }) => {
     const navigation = useNavigation();
@@ -13,20 +14,24 @@ const SectionCard = ({ title, imgUrl }) => {
                     title
                 })
             }}
-            className="bg-white mr-3 mb-3 shadow-xl rounded-xl relative"
+            className="bg-white mr-3 mb-3 shadow-md rounded-xl relative"
         >
 
             <Image source={{
                 uri: "https://media.istockphoto.com/id/94328901/photo/young-woman-with-dog-and-cat.jpg?s=170667a&w=0&k=20&c=l3faNbWec9tEExKlfgDE5vg6EwiUUNnSCKdImfZlJKk="
             }}
-                className="h-48 w-60 rounded-sm object-contain"
+                className="h-48 w-60 rounded-tl-xl rounded-tr-xl object-contain"
             />
-            <View className="p-3 absolute bottom-12 right-6 bg-white rounded-full shadow-sm">
+            <View className="p-3 absolute bottom-16 right-6 bg-white rounded-full shadow-sm">
                 <AntDesign name="hearto" size={20} color="black" />
             </View>
 
-            <View className="px-3 pb-4">
+            <View className="px-5 pb-5">
                 <Text className="font-bold text-lg pt-6">{title}</Text>
+                <View className="flex-row items-center">
+                    <Ionicons name="location-outline" size={16} color="gray" />
+                    <Text className="text-xs text-gray-400">15 km away</Text>
+                </View>
             </View>
         </TouchableOpacity>
     )
