cc0251f471f59f4a0dd4d63b5c68215957ec8a5f
Lena Sokolova <sokolhelena94@gmail.com>
2023-01-11 14:15:23 +0100

    refactor: change the base of the screen + styles

--- screens/ShelterScreen.js
+++ screens/ShelterScreen.js
@@ -1,7 +1,12 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { useLayoutEffect } from 'react';
+import { AntDesign } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import Categories from './../components/Categories';
+import PetCard from '../components/PetCard';
+
 
 const ShelterScreen = () => {
     const navigation = useNavigation();
@@ -12,15 +17,48 @@ const ShelterScreen = () => {
 
     useLayoutEffect(() => {
         navigation.setOptions({
-            headerShown: true,
+            headerShown: false,
         })
     })
     return (
-        <View>
-            <Text>ShelterScreen</Text>
-            <Text>{title}</Text>
+        <ScrollView className='bg-white relative'>
+            <View>
+                <Image source={{
+                    uri: "https://media.istockphoto.com/id/94328901/photo/young-woman-with-dog-and-cat.jpg?s=170667a&w=0&k=20&c=l3faNbWec9tEExKlfgDE5vg6EwiUUNnSCKdImfZlJKk="
+                }}
+                    className="w-full h-72 bg-gray-300 p-4"
+                />
 
-        </View>
+            </View>
+            <TouchableOpacity className="p-3 absolute top-64 right-8 bg-white rounded-full shadow-lg">
+                <AntDesign name="hearto" size={26} color="black" />
+            </TouchableOpacity>
+
+
+            <TouchableOpacity onPress={navigation.goBack} className="p-2 absolute left-8 top-8 bg-green-900 rounded-full">
+                <Ionicons name="chevron-back" size={24} color="white" />
+            </TouchableOpacity>
+            <TouchableOpacity className="p-2 absolute right-8 top-10  bg-green-900 rounded-full">
+                <Ionicons name="share-social-outline" size={24} color="white" />
+
+            </TouchableOpacity>
+
+            <View className="p-5">
+                <Text className="font-bold text-2xl">{title}</Text>
+                <Text className="font-bold text-lg">Description</Text>
+                <Text className="text-s text-gray-600 font-thin">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, fuga! Quod in accusamus labore dolores quo voluptas, accusantium saepe eius quas, nam quae! Assumenda ullam dignissimos temporibus. </Text>
+            </View>
+            <View className="pl-5">
+                <Categories />
+            </View>
+            <ScrollView className="p-5">
+                <PetCard />
+                <PetCard />
+
+
+            </ScrollView>
+
+        </ScrollView>
     )
 }
 
