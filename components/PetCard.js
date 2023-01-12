c232a9dd44fbf0f69eb40f17c05c46fe99a56d90
Lena Sokolova <sokolhelena94@gmail.com>
2023-01-12 16:56:54 +0100

    refactor: pass props, change to conditional rendering, style changes

--- components/PetCard.js
+++ components/PetCard.js
@@ -5,48 +5,55 @@ import { AntDesign } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-const PetCard = () => {
+const PetCard = ({ name, age, img, city, gender, size, tag, info, species, postcode, state, country }) => {
     const navigation = useNavigation();
 
     return (
         <TouchableOpacity
             onPress={() => {
                 navigation.navigate('Pet', {
+                    name, age, img, city, gender, size, tag, info, species, postcode, state, country
                 })
             }}
             className="bg-white shadow-md rounded-2xl relative mb-5"
         >
-            <Image source={{
-                uri: "https://images.unsplash.com/photo-1615751072497-5f5169febe17?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y3V0ZSUyMGRvZ3xlbnwwfHwwfHw%3D&w=1000&q=80"
+            {img ? <Image source={{
+                uri: img
             }}
-                className="h-48 w-full rounded-tl-2xl rounded-tr-2xl object-contain"
-            />
+                className="h-72 w-full rounded-tl-2xl rounded-tr-2xl object-contain"
+            /> : <Image source={{ uri: 'https://via.placeholder.com/350x350/166534/ffffff?text=NO+PHOTO+AVAILABLE' }}
+                className="h-72 w-full rounded-tl-2xl rounded-tr-2xl object-contain"
+            />}
+
             <View className="p-3 absolute bottom-24 right-6 bg-white rounded-full shadow-sm">
                 <AntDesign name="hearto" size={20} color="black" />
             </View>
 
             <View className="px-5 pb-5">
-                <View className="mb-2">
-                    <Text className="font-bold text-lg pt-6">Doggo <Text className="font-thin text-sm text-gray-500 pt-6">3 years old</Text></Text>
+                <View className="mb-2 flex-row items-center space-x-1">
+                    <Text className="font-bold text-lg pt-6">{name} </Text>
+                    <Text className="font-thin text-lg text-gray-500 pt-6">{age}</Text>
+                    <Text className="font-bold text-lg text-gray-800 pt-6 pl-2">{species}</Text>
                 </View>
 
                 <View className="flex-row items-center ">
-                    <Ionicons name="location-outline" size={16} color="gray" />
-                    <Text className="text-xs text-gray-400">15 km away</Text>
+                    <Ionicons name="location-outline" size={16} color="#166534" />
+                    <Text className="text-xs text-gray-400">{city}, {state}, {country} 15 km away</Text>
                 </View>
                 <View className="flex-row mt-2 space-x-5">
                     <View className="flex-row items-center">
-                        <Ionicons name="male" size={20} color="#166534" />
-                        <Text className="text-sm text-gray-700 pl-1">Male</Text>
+                        {gender == 'Male' ? <Ionicons name="male" size={20} color="#166534" /> : <Ionicons name="female" size={20} color="#166534" />}
+                        {/* <Ionicons name="male" size={20} color="#166534" /> */}
+                        <Text className="text-sm text-gray-700 pl-1">{gender}</Text>
                     </View>
                     <View className="flex-row items-center">
 
                         <MaterialCommunityIcons name="weight" size={19} color="#166534" />
-                        <Text className="text-sm text-gray-700 pl-1">Medium</Text>
+                        <Text className="text-sm text-gray-700 pl-1">{size}</Text>
                     </View>
                     <View className="flex-row items-center">
                         <Ionicons name="basketball-outline" size={20} color="#166534" />
-                        <Text className="text-sm text-gray-700 pl-1">Male</Text>
+                        {tag ? <Text className="text-sm text-gray-700 pl-1">{tag}</Text> : <Text className="text-sm text-gray-700 pl-1">Sensitive</Text>}
                     </View>
 
                 </View>
