71e421d6245022780d2a0c503511080a41dcb0fe
Lena Sokolova <sokolhelena94@gmail.com>
2023-01-16 08:46:08 +0100

    refactor: render PetCard comp +render ToPetLocation comp

--- screens/PetScreen.js
+++ screens/PetScreen.js
@@ -5,13 +5,18 @@ import { useLayoutEffect } from 'react';
 import { AntDesign } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import PetCard from '../components/PetCard';
+import ToPetLocation from '../components/ToPetLocation';
+
 
 const PetScreen = () => {
     const navigation = useNavigation();
 
     const { params: {
-        name, age, img, location, gender, size, tag, info, species
+        name, age, img, city, gender, size, tag, info, species, postcode, state, country, addressFirst, addressSecond
     } } = useRoute();
+
+    const url = `https://www.google.com/maps/place/${city},+${state}+${postcode},+${country}`
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -20,17 +25,22 @@ const PetScreen = () => {
     }, []);
     return (
         <ScrollView className='bg-white relative'>
-            <View>
-                <Image source={{
-                    uri: img
-                }}
-                    className="w-full h-72 bg-gray-300 p-4"
-                />
-
-            </View>
-            <TouchableOpacity className="p-3 absolute top-64 right-8 bg-white rounded-full shadow-lg">
-                <AntDesign name="hearto" size={26} color="black" />
-            </TouchableOpacity>
+            <PetCard
+                name={name}
+                age={age}
+                img={img}
+                gender={gender}
+                size={size}
+                tag={tag}
+                species={species}
+                addressFirst={addressFirst}
+                addressSecond={addressSecond}
+                country={country}
+                city={city}
+                info={info}
+                state={state}
+                postcode={postcode}
+            />
 
 
             <TouchableOpacity onPress={navigation.goBack} className="p-2 absolute left-8 top-8 bg-green-900 rounded-full">
@@ -40,33 +50,9 @@ const PetScreen = () => {
                 <Ionicons name="share-social-outline" size={24} color="white" />
             </TouchableOpacity>
             <ScrollView className="px-5 pb-5">
-                <View className="mb-2 flex-row items-center space-x-1">
-                    <Text className="font-bold text-lg pt-6">{name} </Text>
-                    <Text className="font-thin text-lg text-gray-500 pt-6">{age}</Text>
-                    <Text className="font-bold text-lg text-gray-800 pt-6 pl-2">{species}</Text>
-                </View>
 
-                <View className="flex-row items-center ">
-                    <Ionicons name="location-outline" size={16} color="gray" />
-                    <Text className="text-xs text-gray-400">{location} 15 km away</Text>
-                </View>
-                <View className="flex-row mt-2 mb-5 space-x-5">
-                    <View className="flex-row items-center">
-                        <Ionicons name="male" size={20} color="#166534" />
-                        <Text className="text-sm text-gray-700 pl-1">{gender}</Text>
-                    </View>
-                    <View className="flex-row items-center">
-
-                        <MaterialCommunityIcons name="weight" size={19} color="#166534" />
-                        <Text className="text-sm text-gray-700 pl-1">{size}</Text>
-                    </View>
-                    <View className="flex-row items-center">
-                        <Ionicons name="basketball-outline" size={20} color="#166534" />
-                        <Text className="text-sm text-gray-700 pl-1">{tag}</Text>
-                    </View>
-                </View>
                 <View className="mb-8">
-                    <Text>{info}</Text>
+                    {!info ? <Text>Lovely friend!</Text> : <Text>{info}</Text>}
                 </View>
                 <View className="flex space-y-3 mb-8">
                     <View className="flex-row items-center">
@@ -92,29 +78,27 @@ const PetScreen = () => {
                     </View>
                 </View>
 
-                <View>
-                    <Text>I'm here now</Text>
-                    <Text>Animal Shelter Name</Text>
-                    <Text>{location}</Text>
+                <View className="mb-8">
+                    <Text className="text-xl">I'm here now</Text>
+                    <Text className="text-xl font-bold">Animal Shelter Name</Text>
+                    <Text>{addressFirst || addressSecond || "Ask about address"}, {city}, {postcode}, {state}, {country} </Text>
                 </View>
 
-                <View className="bg-slate-500">
-                    <Text className="p-30 ">Here is a map</Text>
-                </View>
-                <View>
-                    <Text>Any question?</Text>
-                </View>
+                <ToPetLocation url={url} children="FIND ME ON A MAP!" img={img} />
+
+
+
             </ScrollView>
-
-
-
 
             <TouchableOpacity className="px-5 pb-5" onPress={() => {
                 alert('To animal shelter contact page!')
             }}>
-                <Text className="px-5 py-5 bg-green-900 text-white text-center rounded-full text-lg">Adopt Me!</Text>
+                {!addressFirst && !addressSecond ? <Text className="px-5 py-5 bg-green-900 text-white text-center rounded-full text-lg">Ask about address!</Text> : <Text className="px-5 py-5 bg-green-900 text-white text-center rounded-full text-lg">Contact & Adopt!</Text>}
+
 
             </TouchableOpacity>
+
+
 
         </ScrollView >
     )
