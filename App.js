7556415aae68dea4e5343342ae2e1afe2d2cf92c
Lena Sokolova <sokolhelena94@gmail.com>
2023-01-16 17:27:16 +0100

    refactor: change the screen name to FormsScreen

--- App.js
+++ App.js
@@ -3,8 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import ShelterScreen from './screens/ShelterScreen';
 import PetScreen from './screens/PetScreen';
-import SignUpScreen from './screens/SignUpScreen';
-import SignInScreen from './screens/SignInScreen';
+import FormsScreen from './screens/FormsScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,8 +12,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         {/* Screens */}
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
-        <Stack.Screen name="SignIn" component={SignInScreen} />
+        <Stack.Screen name="Forms" component={FormsScreen} />
 
 
         <Stack.Screen name="Home" component={HomeScreen} />
