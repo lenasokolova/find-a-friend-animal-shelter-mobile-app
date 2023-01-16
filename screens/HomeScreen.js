0dab4b1fe6d5b9e944d7d0cadd1efbc29c701810
Lena Sokolova <sokolhelena94@gmail.com>
2023-01-16 08:48:05 +0100

    refactor: delete comments + unused code

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, Image } from 'react-native'
+import { View, Text, TextInput, Image, ScrollView } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -11,28 +11,41 @@ const HomeScreen = () => {
 
     // useLayoutEffect for UI to be loaded
     useLayoutEffect(() => {
-        // setOptions allows us to change a navigation options like headerTitle on navbar et.
         navigation.setOptions({
-            // headerTitle: "TESTING",
             headerShown: false,
         })
     }, []);
+
+
     return (
         <SafeAreaView className='bg-white my-5 p-5'>
-            <View className='flex-row items-center space-x-2 pb-2'>
-                <Image source={{
-                    uri: 'https://media.istockphoto.com/id/1357723739/photo/studio-portrait-of-a-smiling-young-latin-woman.jpg?b=1&s=170667a&w=0&k=20&c=RIMvJI9S1mZytKJydukxUF4hRoyVbR1W3ix6gsdo72I='
-                }}
-                    className='h-7 w-7 bg-gray-300 p-5 rounded-full'
-                />
-                <TextInput placeholder='Enter your location...' keyboardType='default' />
-            </View>
-            <View>
-                <Categories />
-            </View>
-            <View>
-                <Sections />
-            </View>
+            <ScrollView>
+                <View className='flex-row items-center space-x-2 pb-2'>
+                    <Image source={{
+                        uri: 'https://media.istockphoto.com/id/1357723739/photo/studio-portrait-of-a-smiling-young-latin-woman.jpg?b=1&s=170667a&w=0&k=20&c=RIMvJI9S1mZytKJydukxUF4hRoyVbR1W3ix6gsdo72I='
+                    }}
+                        className='h-7 w-7 bg-gray-300 p-5 rounded-full'
+                    />
+                    <TextInput placeholder='Enter your location...' keyboardType='default' />
+                </View>
+                <View>
+                    <Categories />
+                </View>
+                <View>
+                    <Sections
+                        section="Shelters near you"
+                        description="Here you can find animal shelters in your area"
+                    />
+                    <Sections
+                        section="Newcommers in your area"
+                        description="Here you can find new came animals nearby"
+                    />
+                </View>
+
+
+
+            </ScrollView>
+
         </SafeAreaView>
 
     )
