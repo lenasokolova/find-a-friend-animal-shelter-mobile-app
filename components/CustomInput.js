5ae576577b12203ab775af2bce10f23db57e56d0
Lena Sokolova <sokolhelena94@gmail.com>
2023-01-16 15:28:43 +0100

    feat: create CustomInput

---
+++ components/CustomInput.js
@@ -0,0 +1,34 @@
+import { Text, TextInput } from 'react-native';
+import React from 'react';
+
+const CustomInput = (props) => {
+
+    const {
+        field: { name, onBlur, onChange, value },
+        form: { errors, touched, setFieldTouched },
+        ...inputProps
+    } = props;
+
+    const hasError = errors[name] && touched[name];
+    const errorBorder = hasError ? 'border-red-700' : 'border-gray-500'
+
+    return (
+        <>
+            <TextInput
+                className={`w-full h-12 my-3 bg-white border rounded-md
+                ${errorBorder}`}
+
+                value={value}
+                onChangeText={(text) => onChange(name)(text)}
+                onBlur={() => {
+                    setFieldTouched(name)
+                    onBlur(name)
+                }}
+                {...inputProps}
+            />
+            {hasError && <Text className="text-md text-red-700 font-semibold">{errors[name]}</Text>}
+        </>
+    )
+}
+
+export default CustomInput
