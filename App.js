260288757b8eee0e1cd25196c9d203009b7fa053
Lena Sokolova <sokolhelena94@gmail.com>
2023-01-16 15:27:36 +0100

    feat: add SignUpScreen + SignInScreen

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import ShelterScreen from './screens/ShelterScreen';
 import PetScreen from './screens/PetScreen';
+import SignUpScreen from './screens/SignUpScreen';
+import SignInScreen from './screens/SignInScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,9 +13,15 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         {/* Screens */}
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="SignIn" component={SignInScreen} />
+
+
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Shelter" component={ShelterScreen} />
         <Stack.Screen name="Pet" component={PetScreen} />
+
+
 
       </Stack.Navigator>
 
