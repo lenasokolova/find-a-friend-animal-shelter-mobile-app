13d9e61e7f9820f2efffda79953109ea243182c7
Lena Sokolova <sokolhelena94@gmail.com>
2023-01-18 16:04:32 +0100

    feat: add SettingsScreen & AddPetScreen

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import HomeScreen from './screens/HomeScreen';
 import ShelterScreen from './screens/ShelterScreen';
 import PetScreen from './screens/PetScreen';
 import FormsScreen from './screens/FormsScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import AddPetToShelterScreen from './screens/AddPetToShelterScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,10 +14,15 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         {/* Screens */}
-        <Stack.Screen name="Forms" component={FormsScreen} />
-
-
+        {/* <Stack.Screen name="Forms" component={FormsScreen} /> */}
         <Stack.Screen name="Home" component={HomeScreen} />
+
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+        <Stack.Screen name="AddPet" component={AddPetToShelterScreen} />
+
+
+
+
         <Stack.Screen name="Shelter" component={ShelterScreen} />
         <Stack.Screen name="Pet" component={PetScreen} />
 
