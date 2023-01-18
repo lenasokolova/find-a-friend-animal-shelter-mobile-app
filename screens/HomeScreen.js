b3f7eec8b4ded92f19b96f16969a7f639f1bd17a
Lena Sokolova <sokolhelena94@gmail.com>
2023-01-18 16:06:34 +0100

    refactor: add navigation from the Image to SettingsScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,6 @@
-import { View, Text, TextInput, Image, ScrollView } from 'react-native'
+import { View, Text, TextInput, Image, ScrollView, TouchableHighlight, TouchableOpacity } from 'react-native'
 import React, { useLayoutEffect } from 'react'
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Categories from '../components/Categories';
 import Sections from '../components/Sections';
@@ -8,6 +8,11 @@ import Sections from '../components/Sections';
 const HomeScreen = () => {
 
     const navigation = useNavigation();
+    // const { params: {
+    //     ref
+    // } } = useRoute();
+
+
 
     // useLayoutEffect for UI to be loaded
     useLayoutEffect(() => {
@@ -21,12 +26,17 @@ const HomeScreen = () => {
         <SafeAreaView className='bg-white my-5 p-5'>
             <ScrollView>
                 <View className='flex-row items-center space-x-2 pb-2'>
-                    <Image source={{
-                        uri: 'https://media.istockphoto.com/id/1357723739/photo/studio-portrait-of-a-smiling-young-latin-woman.jpg?b=1&s=170667a&w=0&k=20&c=RIMvJI9S1mZytKJydukxUF4hRoyVbR1W3ix6gsdo72I='
-                    }}
-                        className='h-7 w-7 bg-gray-300 p-5 rounded-full'
-                    />
+                    <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+                        <Image source={{
+                            uri: 'https://media.istockphoto.com/id/1357723739/photo/studio-portrait-of-a-smiling-young-latin-woman.jpg?b=1&s=170667a&w=0&k=20&c=RIMvJI9S1mZytKJydukxUF4hRoyVbR1W3ix6gsdo72I='
+                        }}
+                            className='h-7 w-7 p-5 rounded-full'
+                        />
+                    </TouchableOpacity>
                     <TextInput placeholder='Enter your location...' keyboardType='default' />
+                </View>
+                <View>
+                    {/* <Text>{ref.current.values.fullName}</Text> */}
                 </View>
                 <View>
                     <Categories />
