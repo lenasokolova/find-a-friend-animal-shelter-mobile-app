f5ab533292cbf6c771d6512657c815af868d1e0b
Lena Sokolova <sokolhelena94@gmail.com>
2023-01-29 16:48:01 +0100

    refactor: change structure  styles

--- components/CustomInput.js
+++ components/CustomInput.js
@@ -1,23 +1,22 @@
-import { Text, TextInput } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 import React from 'react';
 
 const CustomInput = (props) => {
 
-
-    const {
-        field: { name, onBlur, onChange, value },
-        form: { errors, touched, setFieldTouched },
-        ...inputProps
-    } = props;
-
-    const hasError = errors[name] && touched[name];
-    const errorBorder = hasError ? 'border-red-700' : 'border-gray-500'
+    const { label, placeholder, error } = props;
 
     return (
-        <>
-            <TextInput
+        <View className="mb-1/2">
+            <View>
+                <Text className="text-xs mt-1 mb-1">{label}</Text>
+            </View>
+            <TextInput placeholder={placeholder}
+                className="border py-0.5 px-2 rounded-sm border-gray-500 mb-1" />
+            <Text className="text-red-500 text-sm">Some error</Text>
+            {error && <Text>{error}</Text>}
+            {/* <TextInput
                 style={[props.multiline && { height: props.numberOfLines * 40, textAlignVertical: 'top', paddingTop: 12 }]}
-                className={`w-full h-12 my-3 pl-5 bg-white border rounded-md
+                className={`w-full h-12 my-2 pl-5 bg-white border rounded-md
                 ${errorBorder}`}
 
                 value={value}
@@ -28,8 +27,8 @@ const CustomInput = (props) => {
                 }}
                 {...inputProps}
             />
-            {hasError && <Text className="text-md text-red-700 font-semibold">{errors[name]}</Text>}
-        </>
+            {hasError && <Text className="text-md text-red-700 font-semibold">{errors[name]}</Text>} */}
+        </View>
     )
 }
 
